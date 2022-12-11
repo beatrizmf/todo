dccd2a76146a51df33000309bee2531310dcd42c
Beatriz Felix <mbeatrizmfelix@gmail.com>
2022-12-11 12:39:59 -0300

    feat: header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Header } from './components/Header'
 import './global.css'
 
 function App () {
   return (
-    <div className="App">
-     App
-    </div>
+    <>
+     <Header />
+    </>
   )
 }
 
